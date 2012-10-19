bea1f919708a4466ad6e3b4587639e5cb6b76eed
Ilya Vorontsov <prijutme4ty@gmail.com>
2012-10-19 04:39:39 +0400

    specs for existing modifiers added

---
+++ spec/modifier_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper.rb'
+
+describe StupidPassword::Modifiers do
+  it "detects capitalized names" do
+    'Alexander'.is_stupid?.should eq "Is that a capitalized male name?"
+    'Rnieciunm'.is_stupid?.should eq false
+  end
+  it "detects downcase names" do
+    'alexander'.is_stupid?.should eq "Is that a male name in lower case?"
+    'rnieciunm'.is_stupid?.should eq false
+  end
+  it "detects upcase names" do
+    'ALEXANDER'.is_stupid?.should eq "Is that a male name in upper case?"
+    'RNIECIUNM'.is_stupid?.should eq false
+  end
+
+end
